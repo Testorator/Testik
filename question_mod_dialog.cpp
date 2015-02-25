3f9efca755def59a48ae1b2c548a8955305be955
NordOrK <nord_ork@mail.ru>
2015-02-26 00:34:04 +0300

    add elements to ui for question_mod_dialog add toolbar

--- question_mod_dialog.cpp
+++ question_mod_dialog.cpp
@@ -1,11 +1,20 @@
 #include "question_mod_dialog.h"
 #include "ui_question_mod_dialog.h"
 
+#include <QToolBar>
+
 question_mod_dialog::question_mod_dialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::question_mod_dialog)
 {
     ui->setupUi(this);
+    QToolBar* ptb = new QToolBar("answers toolbar");
+    ptb->addAction(tr("Add"));
+    ptb->addSeparator();
+    ptb->addAction(tr("Save"));
+    ptb->addSeparator();
+    ptb->addAction(tr("Del"));
+    ui->gridLayout_Answers_tb->addWidget(ptb,0,0,0,2,Qt::AlignTop);
 }
 
 question_mod_dialog::~question_mod_dialog()
